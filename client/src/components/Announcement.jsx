d05e6321d3862fb46c2f5a35a3621409d6f324b7
dongjay00 <cjswodlehdgn@gmail.com>
2021-10-18 22:34:15 +0900

    [feat/FE] Add homepage slider

--- client/src/components/Announcement.jsx
+++ client/src/components/Announcement.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Container = styled.div`
   height: 30px;
@@ -9,14 +9,14 @@ const Container = styled.div`
   justify-content: center;
   font-size: 14px;
   font-weight: 500;
-`
+`;
 
 const Announcement = () => {
   return (
     <Container>
       Super Deal! Free Shipping on Orders Over $50
     </Container>
-  )
-}
+  );
+};
 
-export default Announcement
+export default Announcement;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,29 @@
-import { Badge } from '@material-ui/core'
-import { Search, ShoppingCartOutlined } from '@material-ui/icons'
-import React from 'react'
-import styled from 'styled-components'
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import React from 'react';
+import styled from 'styled-components';
 
 const Container = styled.div`
   height: 60px;
-`
+`;
 
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-`
+`;
 
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
-`
+`;
 
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
-`
+`;
 
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
@@ -31,33 +31,33 @@ const SearchContainer = styled.div`
   align-items: center;
   margin-left: 25px;
   padding: 5px;
-`
+`;
 
 const Input = styled.input`
   border: none;
-`
+`;
 
 const Center = styled.div`
   flex: 1;
   text-align: center;
-`
+`;
 
 const Logo = styled.h1`
   font-weight: bold;
-`
+`;
 
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
-`
+`;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
-`
+`;
 
 const Navbar = () => {
   return (
@@ -82,7 +82,7 @@ const Navbar = () => {
         </Right>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
