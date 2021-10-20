5527427a9e65ca0f9b7f920da3e795455e4a70ee
dongjay00 <cjswodlehdgn@gmail.com>
2021-10-20 19:55:27 +0900

    [feat/BE] Add getAllProducts api

--- api/routes/product.js
+++ api/routes/product.js
@@ -51,4 +51,27 @@ router.get("/find/:id", async (req, res) => {
   }
 });
 
+// 모든 상품 가져오기
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
