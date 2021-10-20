ce0133776c91975f1f85866b8837c3b991318758
dongjay00 <cjswodlehdgn@gmail.com>
2021-10-20 19:02:33 +0900

    [feat/BE] Add delete product api

--- api/routes/product.js
+++ api/routes/product.js
@@ -31,4 +31,14 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+// 상품 삭제
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
