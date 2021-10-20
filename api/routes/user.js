b71fe698b59be4dd7402de82883c86e5fe80a06a
dongjay00 <cjswodlehdgn@gmail.com>
2021-10-20 16:12:15 +0900

    [feat/BE] Add getAllUsers api for admin

--- api/routes/user.js
+++ api/routes/user.js
@@ -52,4 +52,17 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+// 모든 회원정보 가져오기 (Admin 계정 전용)
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
