9c736492d2301c254cb41055f066e5d8f5657475
dongjay00 <cjswodlehdgn@gmail.com>
2021-10-20 15:41:30 +0900

    [feat/BE] Add delete user api

--- api/routes/user.js
+++ api/routes/user.js
@@ -27,4 +27,14 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+// 회원탈퇴
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
