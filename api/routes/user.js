246444f148e5c0dd3a70e8877671eac3ed60a8e7
dongjay00 <cjswodlehdgn@gmail.com>
2021-10-20 17:48:49 +0900

    [feat/BE] Add stats api for admin

--- api/routes/user.js
+++ api/routes/user.js
@@ -65,4 +65,30 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+// 회원 스탯 정보 가져오기 (Admin 계정 전용)
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
