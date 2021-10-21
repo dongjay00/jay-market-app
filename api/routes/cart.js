6585706aac4122de6e1b2eacd86614f62085e5c3
dongjay00 <cjswodlehdgn@gmail.com>
2021-10-21 09:37:59 +0900

    [feat/BE] Add create cart api

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -1,3 +1,18 @@
+const Cart = require("../models/Cart");
+const { verifyToken } = require("./verifyToken");
+
 const router = require("express").Router();
+
+// 새 카트 생성
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new Cart(req.body);
+
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
