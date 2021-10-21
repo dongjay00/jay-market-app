3b77d7e2e75b9bcb56febd3e9157497d993334c6
dongjay00 <cjswodlehdgn@gmail.com>
2021-10-21 10:48:14 +0900

    [feat/BE] Add update order api

--- api/routes/order.js
+++ api/routes/order.js
@@ -19,4 +19,20 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
+// 주문내역 수정
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
